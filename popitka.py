16b891acf72837eeb6516fe781e6180f45be97b5
Artyom <vonmakaro00@gmail.com>
2022-12-08 03:57:29 +1000

    Добавил словарь как инфо о действиях

--- popitka.py
+++ popitka.py
@@ -21,24 +21,6 @@ def calculator():
             calcresult = (x + y)
             math_dir["Сумма"].append(calcresult)
     return calcresult
-def math_array():
-    array_length=len(array)
-    mathDo = (input("Введите действие "))
-    Elements = int()
-    for i in range(array_length):
-        if mathDo == "+":
-            Elements=sum(array)
-            math_dir["Сумма всех чисел"]=[Elements]
-        elif mathDo == "*":
-            Elements=math.prod(array)
-            math_dir["Умножение всех чисел"]=[Elements]
-        elif mathDo == "/":
-            Elements = array[0] / array[i]
-            math_dir["Деление всех чисел"]=[Elements]
-        elif mathDo == "-":
-            Elements = array[0] - array[i]
-            math_dir["Вычитание всех чисел"]= [Elements]
-    return Elements
 array_size = int(input('Сколько операций?'))
 array = []
 while array !=0:
@@ -48,6 +30,5 @@ while array !=0:
     if array_size == 0:
         break
 array_length=len(array)
-Elements = math_array()
 for k, v in math_dir.items():
     print(f"{k} >>> {v}")
